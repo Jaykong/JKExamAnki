63765bd7d57f675fd86bdbecad50a8d241ac5c60
Jaykong <kongyunpeng2011@sina.com>
2016-04-08 16:33:14 +0800

    First init

---
+++ JKExamAnki/EAPaperInfoViewController.h
@@ -0,0 +1,13 @@
+//
+//  EAPaperInfoViewController.h
+//  JKExamAnki
+//
+//  Created by trainer on 4/8/16.
+//  Copyright © 2016 trainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EAPaperInfoViewController : UIViewController
+
+@end
---
+++ JKExamAnki/EAShareViewController.h
@@ -0,0 +1,13 @@
+//
+//  EAShareViewController.h
+//  JKExamAnki
+//
+//  Created by trainer on 4/8/16.
+//  Copyright © 2016 trainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EAShareViewController : UIViewController
+
+@end
