abee644dc22d6d97497398a8fb3bdbc580ce74ed
Jaykong <kongyunpeng2011@sina.com>
2016-04-08 15:58:58 +0800

    Initial Commit

---
+++ JKExamAnki/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JKExamAnki
+//
+//  Created by trainer on 4/8/16.
+//  Copyright © 2016 trainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JKExamAnki/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JKExamAnki
+//
+//  Created by trainer on 4/8/16.
+//  Copyright © 2016 trainer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
